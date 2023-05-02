b8060a6823597cafeebe8d001dc8af71f9aff26b
trannois <harold.trannois@u-picardie.fr>
2023-05-02 22:39:33 +0200

    suite

--- test/MinTest.php
+++ test/MinTest.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__."/../../src/UPJV/Validator/Min.php";
+require_once __DIR__."/../src/UPJV/Validator/Min.php";
 
 use PHPUnit\Framework\TestCase;
 use PHPUnit\Framework\Attributes\CoversClass;
