e25789bb9f98229b00e1327ab7d208e2bfe0ab1e
trannois <harold.trannois@u-picardie.fr>
2023-05-02 22:33:37 +0200

    suite

---
+++ test/MinTest.php
@@ -0,0 +1,12 @@
+<?php
+require_once __DIR__."/../../src/UPJV/Validator/Min.php";
+
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\Test;
+
+#[CoversClass("Validator")]
+final class MinTest extends TestCase
+{
+
+}
