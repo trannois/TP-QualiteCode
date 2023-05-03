bb7aa3ca387dd402da811f57d71f5eb1dd0853ce
mosleh48 <mosleh.hulk@gmail.com>
2023-05-03 11:40:28 +0200

    commit de mosleh Longueur min deuxieme-V

--- test/phpcs/Symfony/LeMinTest.php
+++ test/phpcs/Symfony/LeMinTest.php
@@ -9,38 +9,28 @@ class LeMinTest extends TestCase
     /**
      * Test if the check method returns true when input value is greater than or equal to the minimum size
      */
-    public function testCheckReturnsTrueForValueGreaterThanOrEqualToSize()
+    public function testCheck()
     {
         // Arrange
         $minSize = 3;
-        $input = 5;
-
+        $input = [5,2];
         // Act
         $validator = new LeMin();
         $validator->build([$minSize]);
         $result = $validator->check($input);
 
-        // Assert
-        $this->assertTrue($result);
+        for ($i = 0; $i <= 1; $i++)
+         {
+            if ($input[$i] == 5 )
+            {
+            // Assert
+            $this->assertTrue($result);
+            }
+            else if($input[$i] == 2 )
+            {
+            // Assert
+            $this->assertFalse($result);
+            }
+         }
     }
-
-
-    /**
-     * Test if the check method returns false when input value is less than the minimum size
-     */
-    public function testCheckReturnsFalseForValueLessThanSize()
-    {
-        // Arrange
-        $minSize = 3;
-        $input = 2;
-
-        // Act
-        $validator = new LeMin();
-        $validator->build([$minSize]);
-        $result = $validator->check($input);
-
-        // Assert
-        $this->assertFalse($result);
-    }
-}
 ?>
