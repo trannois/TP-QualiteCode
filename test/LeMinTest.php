bfe560189f996739cf1636d3e8d1b59d3aa6059f
mosleh48 <mosleh.hulk@gmail.com>
2023-05-03 12:24:39 +0200

    commit de mosleh Longueur min V11

--- test/LeMinTest.php
+++ test/LeMinTest.php
@@ -11,37 +11,23 @@ class LeMinTest extends TestCase
     /**
      * Test if the check method returns true when input value is greater than or equal to the minimum size
      */
-    public function testCheckReturnsTrueForValueGreaterThanOrEqualToSize()
+    public function testCheck()
     {
         // Arrange
         $minSize = 3;
+        $validator = new LeMin();
+        $validator->build([$minSize]);
+
         $input = 5;
-
         // Act
-        $validator = new LeMin();
-        $validator->build([$minSize]);
+ 
         $result = $validator->check($input);
-
-        // Assert
         $this->assertTrue($result);
-    }
 
-    /**
-     * Test if the check method returns false when input value is less than the minimum size
-     */
-    public function testCheckReturnsFalseForValueLessThanSize()
-    {
-        // Arrange
-        $minSize = 3;
-        $input = 2;
-
-        // Act
-        $validator = new LeMin();
-        $validator->build([$minSize]);
+        $input =2;
         $result = $validator->check($input);
-
-        // Assert
         $this->assertFalse($result);
+
     }
 }
 ?>
