f013b77239ef356e6ef7674bca39f340e2ea84dc
jeremmgn <monguin.jeremy@gmail.com>
2023-05-03 12:26:50 +0200

    classe

---
+++ src/UPJV/Validator/EstDuTexte.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Vérifie que la chaîne de caractères ne contient uniquement de l'alphabet
+ */
+
+namespace UPJV\Validator;
+
+/**
+ * Class EstDuTexte implémente les méthodes build & check
+ */
+class EstDuTexte implements ValidatorInterface
+{
+    public function build(array $param): object
+    {
+        return $this;
+    }
+
+    /**
+     * Vérifie que l'input contient uniquement des caractères alphabétiques
+     *
+     * @param $input
+     *
+     * @return bool
+     */
+    public function check($input): bool
+    {
+        return ctype_alpha($input);
+    }
+}
