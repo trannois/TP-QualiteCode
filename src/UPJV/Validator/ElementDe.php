691256e9b7e797236d9cafebd6300aa726d424b0
eriktellier <erik.tellier.etps@gmail.com>
2023-05-03 10:11:18 +0200

    Added ElementDe validator and test

---
+++ src/UPJV/Validator/ElementDe.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * Vérifie qu'un element appartient a un tableau
+ */
+
+namespace UPJV\Validator;
+
+/**
+ * Class ElementDe implémente les méthode build & check
+ */
+class ElementDe implements ValidatorInterface
+{
+    //array of string
+    protected array $tabl = [];
+    
+
+    /**
+     * Récupère le tableau spécifier dans la conf
+     *
+     * @param array $param
+     *
+     * @return object
+     */
+    public function build(array $param): object
+    {
+        $this->tabl = $param;
+
+        return $this;
+    }
+
+    /**
+     * Test l'appartenance de l'élément au tableau
+     *
+     * @param $input
+     *
+     * @return bool
+     */
+    public function check($input): bool
+    {
+        foreach($this->tabl as $element){
+            if($input == $element){
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ test/ElementDeTest.php
@@ -0,0 +1,19 @@
+<?php
+use PHPUnit\Framework\TestCase;
+use UPJV\Validator\ElementDe;
+
+require_once 'src/UPJV/Validator/ElementDe.php';
+
+/*
+* @covers Validateur\Validateur
+*/
+final class ElementDeTest extends TestCase
+{
+    public function testcheck()
+    {
+        $validator = new UPJV\Validator\ElementDe();
+        $validator->build(['test', 'test2']);
+        $this->assertTrue($validator->check('test'));
+        $this->assertFalse($validator->check('t'));    
+    }
+}
